64ebb7aa7b3fe6a50ce610a1bd22a32a368d4e47
nizarj <nizarj@110-55.com>
2020-07-21 21:54:29 +0100

    added new structure for the UI

--- projects/cron-editor/src/lib/cron-editor.module.ts
+++ projects/cron-editor/src/lib/cron-editor.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CronEditorComponent } from './cron-editor.component';
+import { FormsModule } from '@angular/forms';
+import { TimePickerComponent } from './time-picker/time-picker.component';
 
 
 
 @NgModule({
-  declarations: [CronEditorComponent],
-  imports: [
-  ],
+  // declarations: [CronEditorComponent],
+  // imports: [
+  // ],
+  // exports: [CronEditorComponent]
+  declarations: [CronEditorComponent, TimePickerComponent],
+  imports: [CommonModule, FormsModule],
   exports: [CronEditorComponent]
 })
 export class CronEditorModule { }
